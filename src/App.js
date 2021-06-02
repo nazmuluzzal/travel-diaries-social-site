a1bdd4cee2ad0ada106450b259e812dc9850f884
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-06-02 21:47:17 +0600

    created client site and added router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./components/Navbar/Navbar";
+import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from "./components/Screens/Home/Home";
+import Login from "./components/Screens/Login/Login";
+import Signup from "./components/Screens/Signup/Signup";
+import Profile from "./components/Screens/Profile/Profile";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+      <Route path="/signin">
+        <Login></Login>
+      </Route>
+      <Route path="/signup">
+        <Signup></Signup>
+      </Route>
+      <Route path="/profile">
+        <Profile></Profile>
+      </Route>
+    </BrowserRouter>
+
   );
 }
 
