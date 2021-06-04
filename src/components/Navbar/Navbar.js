3bd9e8dcaf21f6be1aacc1d553e66d02d87ac61b
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-06-04 21:49:39 +0600

    changed navigation to home screen

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom'
+import { UserContext } from '../../App'
 const Navbar = () => {
-    return (
-        <nav>
-        <div className="nav-wrapper white" style={{ color:"black"}}>
-          <Link to="/" className="brand-logo left">Instagram</Link>
-          <ul id="nav-mobile" className="right">
-            <li><Link to="/signin">Signin</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/create">Create Post</Link></li>
-          </ul>
-        </div>
-      </nav>
-            
-    );
+  const { state, dispatch } = useContext(UserContext)
+  const renderList = () => {
+    if (state) {
+      return [
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/create">Create Post</Link></li>
+      ]
+    }
+    else {
+      return [
+        <li><Link to="/signin">Signin</Link></li>,
+        <li><Link to="/signup">Sign Up</Link></li>
+      ]
+    }
+  }
+  return (
+    <nav>
+      <div className="nav-wrapper white" style={{ color: "black" }}>
+        <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
+        <ul id="nav-mobile" className="right">
+          {renderList()}
+        </ul>
+      </div>
+    </nav>
+
+  );
 };
 
 export default Navbar;
