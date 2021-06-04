4c6568de9385f480fcbdc80cc7e868e5d5a9b7f5
nazmuluzzal <uzzalnazmul@gmail.com>
2021-06-04 23:55:41 +0600

    [Added] local storage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-    return (
-        <nav>
-        <div className="nav-wrapper white" style={{ color:"black"}}>
-          <Link to="/" className="brand-logo left">Instagram</Link>
-          <ul id="nav-mobile" className="right">
-            <li><Link to="/signin">Signin</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/create">Create Post</Link></li>
-          </ul>
-        </div>
-      </nav>
-            
-    );
+  return (
+    <nav>
+      <div className="nav-wrapper white" style={{ color: "black" }}>
+        <Link to="/" className="brand-logo left">
+          Instagram
+        </Link>
+        <ul id="nav-mobile" className="right">
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/create">Create Post</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
