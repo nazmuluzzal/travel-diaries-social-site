fdf4120e8e22fe362a768a6274e952f94eae4ad4
nazmuluzzal <uzzalnazmul@gmail.com>
2021-06-05 02:43:15 +0600

    [Added] delete post functionality

--- src/components/Screens/CreatePost/CreatePost.js
+++ src/components/Screens/CreatePost/CreatePost.js
@@ -14,40 +14,7 @@ const CreatePost = () => {
         method: "post",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": "Bearer " + localStorage.getItem("jwt")
-        },
-        body: JSON.stringify({
-          title,
-          body,
-          pic: url,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          // console.log(data);
-          if (data.error) {
-            M.toast({ html: data.error, classes: "#c62828 red darken-3" });
-          } else {
-            M.toast({
-              html: "Created Post Successfully",
-              classes: "#43a047 green darken-1",
-            });
-            history.push("/");
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [url])
-
-  useEffect(() => {
-    if (url) {
-      fetch("/createpost", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Authrization: "Bearer " + localStorage.getItem("jwt"),
+          Authorization: "Bearer " + localStorage.getItem("jwt"),
         },
         body: JSON.stringify({
           title,
