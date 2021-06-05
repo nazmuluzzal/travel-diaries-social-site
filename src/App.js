975f9a8369888483865abbd470e75695aff233cc
PulokSec <riopulok@gmial.com>
2021-06-05 16:45:26 +0600

    implement redux,added reset pass,new pass,userProfile,following posts,conditionla navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import './App.css'
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom'
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Screens/Home/Home";
+import Profile from "./components/Screens/Profile/Profile";
+import SignIn from "./components/Screens/SignIn/SignIn";
+import Signup from "./components/Screens/Signup/Signup";
+import CreatePost from "./components/Screens/CreatePost/CreatePost";
+import { Provider, useDispatch } from "react-redux";
+import store from "./redux/store"
+import {loggedUser} from "./redux/action/userAction"
+import UserProfile from "./components/Screens/UserProfile/UserProfile";
+import SubscribesUserPosts from "./components/Screens/SubscribesUserPosts/SubscribesUserPosts";
+import NewPassword from "./components/Screens/NewPassword";
+import Reset from "./components/Screens/Reset";
+
+const Routing = ()=>{
+  const history = useHistory()
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    if(user){
+      dispatch(loggedUser(user))
+    }else{
+      if(!history.location.pathname.startsWith('/reset'))
+           history.push('/signin')
+    }
+  },[dispatch,history])
+  return(
+    <Switch>
+      <Route exact path="/" >
+      <Home />
+      </Route>
+      <Route path="/signin">
+        <SignIn />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/create">
+        <CreatePost/>
+      </Route>
+      <Route path="/profile/:userid">
+        <UserProfile />
+      </Route>
+      <Route path="/myfollowingpost">
+        <SubscribesUserPosts />
+      </Route>
+      <Route exact path="/reset">
+        <Reset/>
+      </Route>
+      <Route path="/reset/:token">
+        <NewPassword />
+      </Route>
+      
+    </Switch>
+  )
+}
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store = {store}>
+    <BrowserRouter>
+      <Navbar />
+      <Routing />
+    </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,100 @@
+import React,{useRef,useEffect,useState} from 'react'
+import {Link ,useHistory} from 'react-router-dom'
+import {clearUser} from '../../redux/action/userAction'
+import M from 'materialize-css'
+import { useDispatch, useSelector } from 'react-redux'
+const NavBar = ()=>{
+    const  searchModal = useRef(null)
+    const [search,setSearch] = useState('')
+    const [userDetails,setUserDetails] = useState([])
+     const history = useHistory()
+     useEffect(()=>{
+         M.Modal.init(searchModal.current)
+     },[])
+     const user = useSelector((state)=>{
+       return state.users;
+     })
+     const dispatch = useDispatch();
+     const renderList = ()=>{
+       if(user){
+           return [
+            <li key="1"><i  data-target="modal1" className="large material-icons modal-trigger" style={{color:"black"}}>search</i></li>,
+            <li key="2"><Link to="/profile">Profile</Link></li>,
+            <li key="3"><Link to="/create">Create Post</Link></li>,
+            <li key="4"><Link to="/myfollowingpost">My following Posts</Link></li>,
+            <li  key="5">
+             <button className="btn #c62828 red darken-3"
+            onClick={()=>{
+              localStorage.clear()
+              dispatch(clearUser())
+              history.push('/signin')
+            }}
+            >
+                Logout
+            </button>
+            </li>
+         
+            
+           ]
+       }else{
+         return [
+          <li  key="6"><Link to="/signin">Signin</Link></li>,
+          <li  key="7"><Link to="/signup">Signup</Link></li>
+         
+         ]
+       }
+     }
+
+
+     const fetchUsers = (query)=>{
+        setSearch(query)
+        fetch('/search-users',{
+          method:"post",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify({
+            query
+          })
+        }).then(res=>res.json())
+        .then(results=>{
+          setUserDetails(results.user)
+        })
+     }
+    return(
+        <nav>
+        <div className="nav-wrapper white">
+          <Link to={user?"/":"/signin"} className="brand-logo left">Travel Diaries</Link>
+          <ul id="nav-mobile" className="right">
+             {renderList()}
+  
+          </ul>
+        </div>
+        <div id="modal1" class="modal" ref={searchModal} style={{color:"black"}}>
+          <div className="modal-content">
+          <input
+            type="text"
+            placeholder="search users"
+            value={search}
+            onChange={(e)=>fetchUsers(e.target.value)}
+            />
+             <ul className="collection">
+               {userDetails.map(item=>{
+                 return <Link to={item._id !== user._id?"/profile/"+item._id:"/profile"} onClick={()=>{
+                   M.Modal.getInstance(searchModal.current).close()
+                   setSearch('')
+                 }}><li className="collection-item">{item.email}</li></Link> 
+               })}
+               
+              </ul>
+          </div>
+          <div className="modal-footer">
+            <button className="modal-close waves-effect waves-green btn-flat" onClick={()=>setSearch('')}>close</button>
+          </div>
+        </div>
+      </nav>
+    )
+}
+
+
+export default NavBar
