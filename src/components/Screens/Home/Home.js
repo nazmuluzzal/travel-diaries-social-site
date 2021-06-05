9b8fd41351ef6e3e30b7186c3bdec8db644f5230
nazmuluzzal <uzzalnazmul@gmail.com>
2021-06-05 22:20:20 +0600

    switch

--- src/components/Screens/Home/Home.js
+++ src/components/Screens/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import './App.css'
-import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Screens/Home/Home";
 import Profile from "./components/Screens/Profile/Profile";
@@ -8,29 +8,29 @@ import SignIn from "./components/Screens/SignIn/SignIn";
 import Signup from "./components/Screens/Signup/Signup";
 import CreatePost from "./components/Screens/CreatePost/CreatePost";
 import { Provider, useDispatch } from "react-redux";
-import store from "./redux/store"
-import {loggedUser} from "./redux/action/userAction"
+import store from "./redux/store";
+import { loggedUser } from "./redux/action/userAction";
 import UserProfile from "./components/Screens/UserProfile/UserProfile";
 import SubscribesUserPosts from "./components/Screens/SubscribesUserPosts/SubscribesUserPosts";
 import NewPassword from "./components/Screens/NewPassword";
 import Reset from "./components/Screens/Reset";
 
-const Routing = ()=>{
-  const history = useHistory()
+const Routing = () => {
+  const history = useHistory();
   const dispatch = useDispatch();
-  useEffect(()=>{
-    const user = JSON.parse(localStorage.getItem("user"))
-    if(user){
-      dispatch(loggedUser(user))
-    }else{
-      if(!history.location.pathname.startsWith('/reset'))
-           history.push('/signin')
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch(loggedUser(user));
+    } else {
+      if (!history.location.pathname.startsWith("/reset"))
+        history.push("/signin");
     }
-  },[dispatch,history])
-  return(
+  }, [dispatch, history]);
+  return (
     <Switch>
-      <Route exact path="/" >
-      <Home />
+      <Route exact path="/">
+        <Home />
       </Route>
       <Route path="/signin">
         <SignIn />
@@ -42,7 +42,7 @@ const Routing = ()=>{
         <Profile />
       </Route>
       <Route path="/create">
-        <CreatePost/>
+        <CreatePost />
       </Route>
       <Route path="/profile/:userid">
         <UserProfile />
@@ -51,22 +51,22 @@ const Routing = ()=>{
         <SubscribesUserPosts />
       </Route>
       <Route exact path="/reset">
-        <Reset/>
+        <Reset />
       </Route>
       <Route path="/reset/:token">
         <NewPassword />
       </Route>
     </Switch>
-  )
-}
+  );
+};
 
-function App() {  
+function App() {
   return (
-    <Provider store = {store}>
-    <BrowserRouter>
-      <Navbar />
-      <Routing />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routing />
+      </BrowserRouter>
     </Provider>
   );
 }
